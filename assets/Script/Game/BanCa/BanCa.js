9871453a7527fba6bb50444f48cd2c76643b7e9b
MrT98 <toan.vp98.wapmaster@gmail.com>
2019-11-10 15:30:27 +0700

    update 10/11/2019

--- assets/Script/Game/BanCa/BanCa.js
+++ assets/Script/Game/BanCa/BanCa.js
@@ -1,41 +1,34 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+var helper = require('Helper');
+var notice = require('Notice');
+//var dialog = require('VQRed_dialog');
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+    	nick:    cc.Label,
+    	balans:  cc.Label,
+    	loading: cc.Node,
+    	notice:  notice,
     },
+    onLoad () {
+    	cc.RedT.inGame = this;
+    	cc.RedT.send({scene:"bc"});
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
-    start () {
-
+    	this.nick.string   = cc.RedT.user.name;
+    	this.balans.string = helper.numberWithCommas(cc.RedT.user.red);
     },
-
-    // update (dt) {},
+    onData: function(data) {
+    },
+    backGame: function(){
+		this.loading.active = true;
+		void 0 !== this.timeOut && clearTimeout(this.timeOut);
+		cc.director.loadScene('MainGame');
+	},
+	signOut: function(){
+		cc.director.loadScene('MainGame', function(){
+			cc.RedT.inGame.signOut();
+		});
+	},
 });
