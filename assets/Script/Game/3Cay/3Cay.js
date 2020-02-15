363e8da5d63997c04bb0975843e176c434ecaed1
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-02-15 19:13:13 +0700

    update 15/02/2020

--- assets/Script/Game/3Cay/3Cay.js
+++ assets/Script/Game/3Cay/3Cay.js
@@ -8,6 +8,7 @@ cc.Class({
 
     properties: {
     	gameRoom: cc.Label,
+    	labelTimeStart: cc.Label,
     	nodeNotice: cc.Node,
 		prefabNotice: cc.Prefab,
 		loading:   cc.Node,
@@ -15,19 +16,29 @@ cc.Class({
 		noticeOut: cc.Node,
 		notice:    notice,
 		mePlayer: mePlayer,
+		player: {
+			default: [],
+			type: mePlayer,
+		},
 		panel: false,
 		dataOn: true,
     },
     onLoad(){
     	cc.RedT.inGame = this;
 		cc.RedT.MiniPanel.node.parent = this.redhat;
-		cc.RedT.send({scene:'bacay', g:{bacay:{ingame:true}}});
 
 		this.mePlayer.nickname.string = cc.RedT.user.name;
 		this.mePlayer.balans.string = helper.numberWithCommas(cc.RedT.user.red);
 		this.mePlayer.setAvatar(cc.RedT.user.avatar);
+
+		this.player.forEach(function(player){
+			player.init();
+		});
+
+		cc.RedT.send({scene:'bacay', g:{bacay:{ingame:true}}});
     },
     onData: function(data) {
+    	console.log(data);
 		if (!!data.mini){
 			cc.RedT.MiniPanel.onData(data.mini);
 		}
@@ -44,6 +55,21 @@ cc.Class({
 			cc.RedT.userData(data.user);
 		}
 		if (this.dataOn) {
+			if (!!data.meMap) {
+				this.meMap = data.meMap;
+			}
+			if (!!data.infoGhe) {  // thông tin các ghế
+				this.infoGhe(data.infoGhe);
+			}
+			if (!!data.infoRoom) { // thông tin phòng
+				this.infoRoom(data.infoRoom);
+			}
+			if (!!data.ingame) {  // có người vào phòng
+				this.ingame(data.ingame);
+			}
+			if (!!data.outgame) {  // có người ra khỏi phòng
+				this.outgame(data.outgame);
+			}
 			if (!!data.kick) {
 				this.kick();
 			}
@@ -52,7 +78,80 @@ cc.Class({
 			}
 		}
 	},
-
+	infoGhe: function(info){
+		let player = {};
+		let newGhe = [];
+		if (this.meMap != 1) {
+			let map = this.meMap-1;
+			newGhe = newGhe.concat(info.slice(map),info.slice(0, map));
+		}else{
+			newGhe = info;
+		}
+		newGhe.forEach(function(obj, index){
+			let item = this.player[index];
+			item.map = obj.ghe;
+			player[obj.ghe] = item;
+			item.setInfo(obj.data);
+			return void 0;
+		}.bind(this));
+		this.player = player;
+		player = null;
+		newGhe = null;
+	},
+	infoRoom: function(data){
+		if (data.game !== void 0) {
+			this.gameRoom.string = helper.numberWithCommas(data.game);
+		}
+		if (data.betGa !== void 0) {
+			//this.mainBetGa.string = helper.numberWithCommas(data.bet);
+		}
+		if (data.isStop !== void 0) {
+			//this.labelTimeStart.node.active = false;
+			clearInterval(this.regTime1);
+		}
+		if (data.isPlay == true && data.time_start !== void 0) {
+			/**
+			this.resetGame();
+			this.time_start = data.time_start>>0;
+			this.labelTimeStart.node.active = true;
+			this.labelTimeStart.string = '';
+			this.regTime1 = setInterval(function(){
+				this.labelTimeStart.string = helper.numberPad(this.time_start, 2);
+				if (this.time_start < 0) {
+					this.labelTimeStart.node.active = false;
+					clearInterval(this.regTime1);
+				}
+				this.time_start--;
+			}.bind(this), 1000);
+			*/
+		}
+		if (data.first !== void 0) {
+			/*
+			data.first.forEach(function(player){
+				let get_player = this.player[player.id];
+				get_player.noticeBet(player.bet, '', 2, 22, this.font1);
+				get_player.bet.string = helper.numberWithCommas(player.bet);
+			}.bind(this));
+			*/
+		}
+		if (data.card !== void 0) {
+			data.card.forEach(function(obj){
+				let player = this.player[obj.ghe];
+				if (this.mePlayer !== player) {
+					player.item.forEach(function(item){
+						item.node.active = true;
+						item.spriteFrame = cc.RedT.util.card.cardB1;
+					});
+				}
+			}.bind(this));
+		}
+	},
+	ingame: function(data){
+		this.player[data.ghe].setInfo(data.data);
+	},
+	outgame: function(data){
+		this.player[data].setInfo(null);
+	},
 	kick: function(){
 		cc.RedT.MiniPanel.node.parent = null;
 		this.dataOn = false;
--- assets/Script/Game/3Cay/3Cay_player.js
+++ assets/Script/Game/3Cay/3Cay_player.js
@@ -2,40 +2,116 @@
 var helper = require('Helper');
 
 cc.Class({
-    extends: cc.Component,
+	extends: cc.Component,
 
-    properties: {
-        nickname:    cc.Label,
-        balans:      cc.Label,
-        ic_dealer:   cc.Node,
+	properties: {
+		nickname:    cc.Label,
+		balans:      cc.Label,
+		ic_dealer:   cc.Node,
 
-        nodeDealer:  cc.Node,
-        betDealer:   cc.Label,
+		nodeDealer:  cc.Node,
+		betDealer:   cc.Label,
 
-        nodeChicken: cc.Node,
-        betChicken:  cc.Label,
+		nodeChicken: cc.Node,
+		betChicken:  cc.Label,
 
-        card:     cc.Node,
-        status:   cc.Node,
-        progress: cc.ProgressBar,
-        avatar:   cc.Sprite,
-        isOpen:   false,
-        isAll:    false,
-        isHuy:    false,
-    },
-    init: function(){
-        this.item = this.card.children.map(function(item){
-            item.defaultPosition = item.position;
-            item.defaultAngle    = item.angle;
-            return item.getComponent(cc.Sprite);
-        });
-    },
-    setAvatar: function(data){
-        data = data>>0;
-        if (cc.RedT.avatars[data] !== void 0) {
-            this.avatar.spriteFrame = cc.RedT.avatars[data];
-        }else{
-            this.avatar.spriteFrame = cc.RedT.avatars[0];
-        }
-    },
+		card:     cc.Node,
+		status:   cc.Node,
+		progress: cc.ProgressBar,
+		avatar:   cc.Sprite,
+		isOpen:   false,
+		isAll:    false,
+		isHuy:    false,
+	},
+	init: function(){
+		this.item = this.card.children.map(function(item){
+			item.defaultPosition = item.position;
+			item.defaultAngle    = item.angle;
+			return item.getComponent(cc.Sprite);
+		});
+	},
+	setAvatar: function(data){
+		data = data>>0;
+		if (cc.RedT.avatars[data] !== void 0) {
+			this.avatar.spriteFrame = cc.RedT.avatars[data];
+		}else{
+			this.avatar.spriteFrame = cc.RedT.avatars[0];
+		}
+	},
+	setInfo: function(data, isWin = false){
+		if (!!data) {
+			this.node.active = true;
+			if (data.balans !== void 0) {
+				if (isWin) {
+					this.node.runAction(cc.sequence(
+						cc.delayTime(1),
+						cc.callFunc(function() {
+							this.balans.string = helper.numberWithCommas(data.balans);
+							data = null;
+						}, this),
+					));
+				}else{
+					this.balans.string = helper.numberWithCommas(data.balans);
+				}
+			}
+			!!data.name && (this.nickname.string = data.name);
+			if (!!data.progress) {
+				this.startProgress(data.progress);
+			}
+			if (data.betChuong !== void 0) {
+				this.betDealer.node.active = !!data.betChuong;
+				this.betDealer.string = helper.numberWithCommas(data.betChuong);
+			}
+			if (data.betGa !== void 0) {
+				this.betChicken.node.active = !!data.betGa;
+				this.betChicken.string = helper.numberWithCommas(data.betGa);
+			}
+			if (data.openCard !== void 0 && cc.RedT.inGame.player[cc.RedT.inGame.meMap] !== this) {
+				this.openCard(data.openCard);
+			}
+			if (data.avatar !== void 0) {
+				this.setAvatar(data.avatar);
+			}
+		}else{
+			//this.resetGame();
+			this.node.active = false;
+		}
+	},
+	startProgress: function(time) {
+		this.Progress.progress = 0;
+		this.progressTime = time;
+		this.oldTime  = new Date().getTime();
+		this.isUpdate = true;
+	},
+	setProgress: function(time, progress) {
+		this.Progress.progress = progress;
+		this.progressTime = time;
+		this.oldTime  = new Date().getTime();
+		this.isUpdate = true;
+	},
+	resetGame: function(){
+		this.item.forEach(function(item){
+			item.node.color  = item.node.color.fromHEX('FFFFFF');
+			item.node.active = false;
+			item.bai = null;
+		});
+		this.isAll = false;
+		this.isHuy = false;
+		this.resetStatus();
+		this.bgWin.active = false;
+		this.bet.string = '';
+		this.isOpen = false;
+	},
+	update: function(t){
+		if (this.isUpdate === true) {
+			let h = new Date().getTime();
+			let progress = ((h-this.oldTime)/1000)/this.progressTime;
+			this.Progress.progress = progress+(t/this.progressTime);
+			if (this.Progress.progress >= 1) {
+				this.Progress.progress = 0;
+				this.progressTime = 0;
+				this.isUpdate = false;
+			}
+		}
+	},
 });
